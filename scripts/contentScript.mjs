a3feac0c2db0ca615f7af0e37f7a6accd5a82c9e
Josh Dorn <jr_dorn@yahoo.com>
2022-01-12 17:09:20 -0600

    Capture text input with contentScript

--- scripts/contentScript.mjs
+++ scripts/contentScript.mjs
@@ -14,6 +14,35 @@
 //
 //
 
-// document.body.style.backgroundColor = "red";
+// let input = document.querySelector("input");
+// input.style.color = "red";
+// document.querySelector("input").style.color = "red";
+// document.querySelectorAll("textarea").style.color = "red";
 
-let contents = "contents";
+console.log(1);
+
+//
+// const source = document.getElementById("source");
+// const result = document.getElementById("result");
+
+// const inputHandler = function (e) {
+//   result.innerHTML = e.target.value;
+// };
+
+// source.addEventListener("input", inputHandler);
+//
+
+let display = document.querySelector("input");
+
+// set style per element vs by its content
+// capture all text input, ability to inject content after them
+// copy the entirety of, or sections of, innerHTML and paste
+let inputHandler = (e) => {
+  //change characters to blue font
+  if (e.data !== null) {
+    let char = e.data;
+    return char.fontcolor("blue");
+  }
+};
+
+display.addEventListener("input", inputHandler);
