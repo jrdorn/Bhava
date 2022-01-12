7cfc64f10b416f5738da994775e7bec194c1117e
Josh Dorn <jr_dorn@yahoo.com>
2022-01-12 13:48:19 -0600

    Install tfjs package

---
+++ scripts/popup.mjs
@@ -0,0 +1,25 @@
+/**
+ * 
+ app displayed when extension is active
+ */
+
+// //
+// let changeColor = document.querySelector("#changeColor");
+
+// // setPageBackgroundColor when button is clicked
+// changeColor.addEventListener("click", async () => {
+//   console.log("gm");
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: setPageBackgroundColor,
+//   });
+// });
+
+// //
+// let setPageBackgroundColor = () => {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     document.body.style.backgroundColor = color;
+//   });
+// };
